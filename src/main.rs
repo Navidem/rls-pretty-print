8e8e08e3327ac9b2b6a3e857eeb4d71fdb6d84bd
Navid Emamdoost <nemamdoost@nemamdoost.local>
2018-05-25 10:03:00 -0700

    added default path

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,18 @@ use std::{path, env};
 //use rls_analysis::AnalysisLoader;
 use rls_analysis::{AnalysisHost};
 
-fn main() -> Result< (), Box<std::error::Error> >{
+pub fn main() -> Result< (), Box<std::error::Error> >{
     let args: Vec<String> = env::args().collect();
+    //println!("args len: {}",args.len() );
    // let mut ld = rls_analysis::CargoAnalysisLoader::new(rls_analysis::Target::Debug);
    // ld.set_path_prefix(path::Path::new("."));
     //let host = ld.fresh_host();
     //type Blacklist<'a> = &'a [&'static str];
     let analysis = rls_analysis::AnalysisHost::new(rls_analysis::Target::Debug);
-    let path = path::Path::new(&args[1]);
+    let mut path = path::Path::new(".");
+    if args.len() > 1 {
+        path = path::Path::new(&args[1]);
+    } 
     println!("Wroking path: {:?}", path);
     //let blacklist : Blacklist;
     //analysis.reload_with_blacklist(path, path, &bckList);
@@ -22,15 +26,17 @@ fn main() -> Result< (), Box<std::error::Error> >{
     let roots = analysis.def_roots()?;
 
     //println!("{:?}",roots );
-    for (id, _membr_name) in roots {
+    for (id, membr_name) in roots {
         let def = analysis.get_def(id)?;
+        println!("Root: {:?} {:?} {:?} {}", id, def.kind, def.name, membr_name );
         traverse(id, def , &analysis, 0)?;
     }
     Ok(())
 
 }
 
-fn traverse(id: rls_analysis::Id, defin: rls_analysis::Def ,analysis: &AnalysisHost, mut indent: u32) -> Result < (), Box<std::error::Error>> {
+fn traverse(id: rls_analysis::Id, defin: rls_analysis::Def ,analysis: &AnalysisHost, mut indent: u32) 
+    -> Result < (), Box<std::error::Error>> {
     println!("{} {:?} {:?} {:?}", " ".repeat(indent as usize), id, defin.kind, defin.name);
     indent += 2;
     let children = analysis.for_each_child_def(id, |id, def| (id, def.clone()) )?;
